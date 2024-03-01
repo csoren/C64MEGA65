138723e51c610e2ca1e085f4bde304af0f40b2e7
sy2002 <code@sy2002.de>
2024-03-01 11:17:14 +0100

    V5.1 Release Candidate 1 (RC1)

--- CORE/vhdl/config.vhd
+++ CORE/vhdl/config.vhd
@@ -77,7 +77,7 @@ type WHS_RECORD_ARRAY_TYPE is array (0 to WHS_RECORDS - 1) of WHS_RECORD_TYPE;
 
 constant SCR_WELCOME : string :=
 
-   "\n Commodore 64 for MEGA65 Version 5.1A15\n\n" &
+   "\n Commodore 64 for MEGA65 Version 5.1RC2\n\n" &
 
    " MiSTer port 2024 by MJoergen & sy2002\n" &
    " Powered by MiSTer2MEGA65\n\n\n" &
@@ -97,7 +97,7 @@ constant SCR_WELCOME : string :=
 
 constant HELP_1 : string :=
 
-   "\n Commodore 64 for MEGA65 Version 5.1A15\n\n" &
+   "\n Commodore 64 for MEGA65 Version 5.1RC2\n\n" &
 
    " MiSTer port 2024 by MJoergen & sy2002\n" &
    " Powered by MiSTer2MEGA65\n\n" &
@@ -128,7 +128,7 @@ constant HELP_1 : string :=
 
 constant HELP_2 : string :=
 
-   "\n Commodore 64 for MEGA65 Version 5.1A15\n\n" &
+   "\n Commodore 64 for MEGA65 Version 5.1RC2\n\n" &
 
    " When browsing the menu:\n\n" &
 
@@ -159,7 +159,7 @@ constant HELP_2 : string :=
 
 constant HELP_3 : string :=
 
-   "\n Commodore 64 for MEGA65 Version 5.1A15\n\n" &
+   "\n Commodore 64 for MEGA65 Version 5.1RC2\n\n" &
 
    " SID:\n\n" &
 
@@ -304,7 +304,7 @@ constant SEL_CORENAME      : std_logic_vector(15 downto 0) := x"0200";
 
 -- Currently this is only used in the debug console. Use the welcome screen and the
 -- help system to display the name and version of your core to the end user
-constant CORENAME          : string := "Commodore 64 for MEGA65 Version 5.1A15";
+constant CORENAME          : string := "Commodore 64 for MEGA65 Version 5.1RC2";
 
 --------------------------------------------------------------------------------------------------------------------
 -- "Help" menu / Options menu  (Selectors 0x0300 .. 0x0312): DO NOT TOUCH
